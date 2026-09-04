d5fdab5e73c0727fd9214cc328a023f1c3d7bb70
Bortoleto <ericbortoleto_9@outlook.com>
2026-09-04 20:15:23 -0300

    Implement MoviesRepository and movie section domain models
    - Create `MovieSection` data class and `SectionType` enum to represent categorized movie lists.
    - Implement `toModel()` extension function to map network `MoviesResponse` objects to the `Movie` domain model.
    - Create `MoviesRepository` to handle data fetching and mapping logic from the network layer.
    - Use Kotlin Coroutines with `async` to fetch popular, top-rated, and upcoming movies concurrently.
    - Integrate base image URL formatting into the movie model mapping logic.

--- shared/src/commonMain/kotlin/io/github/bortoletoeric/movies/domain/model/Movie.kt
+++ shared/src/commonMain/kotlin/io/github/bortoletoeric/movies/domain/model/Movie.kt
@@ -1,10 +1,20 @@
 package io.github.bortoletoeric.movies.domain.model
 
+import io.github.bortoletoeric.movies.data.network.IMAGE_SMALL_BASE_URL
+import io.github.bortoletoeric.movies.data.network.model.MoviesResponse
+
 data class Movie(
     val id: Int,
     val title: String,
     val overview: String,
     val posterUrl: String
+)
+
+fun MoviesResponse.toModel() = Movie(
+    id = this.id,
+    title = this.title,
+    overview = this.overview,
+    posterUrl = "$IMAGE_SMALL_BASE_URL${this.posterPath}"
 )
 
 //fake objects
